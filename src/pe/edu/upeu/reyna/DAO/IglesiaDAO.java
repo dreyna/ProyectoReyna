9d760dadb2702dabf12f4aaa0f55cf7d716b86ac
admin-harold.rojas <admin-harold.rojas@FIA-LAB7PROF.lim.upeu.edu.pe>
2014-06-25 16:37:36 -0500

    Version3: Implementacion de DAOs3

--- src/pe/edu/upeu/reyna/DAO/IglesiaDAO.java
+++ src/pe/edu/upeu/reyna/DAO/IglesiaDAO.java
@@ -6,10 +6,32 @@
 
 package pe.edu.upeu.reyna.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import pe.edu.upeu.reyna.config.Conexion;
+
 /**
  *
  * @author admin-harold.rojas
  */
 public class IglesiaDAO {
+    private Connection cx;
+    private Statement st;
+    private ResultSet rs;
+    private String sql;
+    private int op;
+    public int registrarIglesia(int idd, int idti, String nom, int cuenta){
+    sql = "INSERT INTO iglesia VALUES(null,'"+idd+"',"
+            + "'"+idti+"','"+nom+"','"+cuenta+"')";
+        try {
+            cx = Conexion.getConex();
+            st = cx.createStatement();
+            op = st.executeUpdate(sql);
+        } catch (SQLException e) {
+        }
+    return op;
+    }
     
 }
